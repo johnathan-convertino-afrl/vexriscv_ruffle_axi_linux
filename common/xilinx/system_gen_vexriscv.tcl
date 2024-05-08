d0ebbdbbe182572f79545baad532cb3a4ed093cf
Jay Convertino <johnathan.convertino.1@us.af.mil>
2024-05-08 17:41:12 -0400

    first addition of flash controller, rough add, no ports, no wire up, will not work as is.

--- common/xilinx/system_gen_vexriscv.tcl
+++ common/xilinx/system_gen_vexriscv.tcl
@@ -37,7 +37,7 @@ set_property CONFIG.POLARITY ACTIVE_HIGH [get_bd_pins /inst_ruffle_axi_bscane/re
 ip_vlvn_version_check "xilinx.com:ip:axi_interconnect:2.1"
 
 create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 axi_interconnect
-set_property CONFIG.NUM_MI 9 [get_bd_cells axi_interconnect]
+set_property CONFIG.NUM_MI 10 [get_bd_cells axi_interconnect]
 set_property CONFIG.NUM_SI 3 [get_bd_cells axi_interconnect]
 
 ip_vlvn_version_check "xilinx.com:ip:axi_uartlite:2.0"
@@ -61,8 +61,8 @@ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_gpio:2.0 axi_gpio
 
 ip_vlvn_version_check "xilinx.com:ip:axi_quad_spi:3.2"
 
-create_bd_cell -type ip -vlnv xilinx.com:ip:axi_quad_spi:3.2 axi_quad_spi
-set_property CONFIG.C_USE_STARTUP {0} [get_bd_cells axi_quad_spi]
+create_bd_cell -type ip -vlnv xilinx.com:ip:axi_quad_spi:3.2 axi_spi
+set_property CONFIG.C_USE_STARTUP {0} [get_bd_cells axi_spi]
 
 ip_vlvn_version_check "xilinx.com:ip:axi_ethernetlite:3.0"
 
@@ -85,6 +85,12 @@ ip_vlvn_version_check "xilinx.com:ip:xlconstant:1.1"
 
 create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 xlconstant_0
 set_property CONFIG.CONST_VAL {0} [get_bd_cells xlconstant_0]
+
+ip_vlvn_version_check "xilinx.com:ip:axi_quad_spi:3.2"
+
+create_bd_cell -type ip -vlnv xilinx.com:ip:axi_quad_spi:3.2 axi_quad_spi
+set_property CONFIG.C_SPI_MEMORY {3} [get_bd_cells axi_quad_spi]
+set_property CONFIG.C_SPI_MODE {2} [get_bd_cells axi_quad_spi]
 
 create_bd_intf_port -mode Master -vlnv xilinx.com:interface:ddrx_rtl:1.0 DDR
 connect_bd_intf_net [get_bd_intf_pins axi_ddr_ctrl/DDR*] [get_bd_intf_ports DDR]
@@ -117,40 +123,40 @@ create_bd_intf_port -mode Master -vlnv xilinx.com:interface:mdio_rtl:1.0 MDIO
 connect_bd_intf_net [get_bd_intf_pins axi_ethernetlite/MDIO] [get_bd_intf_ports MDIO]
 
 create_bd_port -dir I spi_io0_i
-connect_bd_net [get_bd_pins /axi_quad_spi/io0_i] [get_bd_ports spi_io0_i]
+connect_bd_net [get_bd_pins /axi_spi/io0_i] [get_bd_ports spi_io0_i]
 
 create_bd_port -dir O spi_io0_o
-connect_bd_net [get_bd_pins /axi_quad_spi/io0_o] [get_bd_ports spi_io0_o]
+connect_bd_net [get_bd_pins /axi_spi/io0_o] [get_bd_ports spi_io0_o]
 
 create_bd_port -dir O spi_io0_t
-connect_bd_net [get_bd_pins /axi_quad_spi/io0_t] [get_bd_ports spi_io0_t]
+connect_bd_net [get_bd_pins /axi_spi/io0_t] [get_bd_ports spi_io0_t]
 
 create_bd_port -dir I spi_io1_i
-connect_bd_net [get_bd_pins /axi_quad_spi/io1_i] [get_bd_ports spi_io1_i]
+connect_bd_net [get_bd_pins /axi_spi/io1_i] [get_bd_ports spi_io1_i]
 
 create_bd_port -dir O spi_io1_o
-connect_bd_net [get_bd_pins /axi_quad_spi/io1_o] [get_bd_ports spi_io1_o]
+connect_bd_net [get_bd_pins /axi_spi/io1_o] [get_bd_ports spi_io1_o]
 
 create_bd_port -dir O spi_io1_t
-connect_bd_net [get_bd_pins /axi_quad_spi/io1_t] [get_bd_ports spi_io1_t]
+connect_bd_net [get_bd_pins /axi_spi/io1_t] [get_bd_ports spi_io1_t]
 
 create_bd_port -dir I spi_sck_i
-connect_bd_net [get_bd_pins /axi_quad_spi/sck_i] [get_bd_ports spi_sck_i]
+connect_bd_net [get_bd_pins /axi_spi/sck_i] [get_bd_ports spi_sck_i]
 
 create_bd_port -dir O spi_sck_o
-connect_bd_net [get_bd_pins /axi_quad_spi/sck_o] [get_bd_ports spi_sck_o]
+connect_bd_net [get_bd_pins /axi_spi/sck_o] [get_bd_ports spi_sck_o]
 
 create_bd_port -dir O spi_sck_t
-connect_bd_net [get_bd_pins /axi_quad_spi/sck_t] [get_bd_ports spi_sck_t]
+connect_bd_net [get_bd_pins /axi_spi/sck_t] [get_bd_ports spi_sck_t]
 
 create_bd_port -dir I -from 0 -to 0 spi_ss_i
-connect_bd_net [get_bd_pins /axi_quad_spi/ss_i] [get_bd_ports spi_ss_i]
+connect_bd_net [get_bd_pins /axi_spi/ss_i] [get_bd_ports spi_ss_i]
 
 create_bd_port -dir O -from 0 -to 0 spi_ss_o
-connect_bd_net [get_bd_pins /axi_quad_spi/ss_o] [get_bd_ports spi_ss_o]
+connect_bd_net [get_bd_pins /axi_spi/ss_o] [get_bd_ports spi_ss_o]
 
 create_bd_port -dir O spi_ss_t
-connect_bd_net [get_bd_pins /axi_quad_spi/ss_t] [get_bd_ports spi_ss_t]
+connect_bd_net [get_bd_pins /axi_spi/ss_t] [get_bd_ports spi_ss_t]
 
 connect_bd_net [get_bd_pins clk_wiz_1/clk_out1] [get_bd_pins sys_rstgen/slowest_sync_clk]
 
@@ -177,6 +183,7 @@ connect_bd_net [get_bd_pins axi_interconnect/M06_ACLK] [get_bd_pins clk_wiz_1/cl
 connect_bd_net [get_bd_pins axi_interconnect/M07_ACLK] [get_bd_pins clk_wiz_1/clk_out1]
 # connect_bd_net [get_bd_pins axi_interconnect/M08_ACLK] [get_bd_pins clk_wiz_1/clk_out1]
 connect_bd_net [get_bd_pins axi_interconnect/M08_ACLK] [get_bd_pins axi_ddr_ctrl/ui_clk]
+connect_bd_net [get_bd_pins axi_interconnect/M09_ACLK] [get_bd_pins clk_wiz_1/clk_out1]
 connect_bd_net [get_bd_pins axi_interconnect/ARESETN] [get_bd_pins axi_interconnect/S00_ARESETN] -boundary_type upper
 connect_bd_net [get_bd_pins axi_interconnect/S01_ARESETN] [get_bd_pins axi_interconnect/ARESETN] -boundary_type upper
 connect_bd_net [get_bd_pins axi_interconnect/S02_ARESETN] [get_bd_pins axi_interconnect/ARESETN] -boundary_type upper
@@ -190,6 +197,7 @@ connect_bd_net [get_bd_pins axi_interconnect/M06_ARESETN] [get_bd_pins axi_inter
 connect_bd_net [get_bd_pins axi_interconnect/M07_ARESETN] [get_bd_pins axi_interconnect/ARESETN] -boundary_type upper
 # connect_bd_net [get_bd_pins axi_interconnect/M08_ARESETN] [get_bd_pins axi_interconnect/ARESETN] -boundary_type upper
 connect_bd_net [get_bd_pins axi_interconnect/M08_ARESETN] [get_bd_pins ddr_rstgen/interconnect_aresetn]
+connect_bd_net [get_bd_pins axi_interconnect/M09_ARESETN] [get_bd_pins axi_interconnect/ARESETN] -boundary_type upper
 connect_bd_net [get_bd_pins sys_rstgen/interconnect_aresetn] [get_bd_pins axi_interconnect/ARESETN]
 
 connect_bd_net [get_bd_pins axi_uartlite/s_axi_aclk] [get_bd_pins clk_wiz_1/clk_out1]
@@ -207,10 +215,10 @@ connect_bd_intf_net [get_bd_intf_pins axi_gpio/S_AXI] -boundary_type upper [get_
 connect_bd_net [get_bd_pins axi_gpio/s_axi_aclk] [get_bd_pins clk_wiz_1/clk_out1]
 connect_bd_net [get_bd_pins axi_gpio/s_axi_aresetn] [get_bd_pins sys_rstgen/peripheral_aresetn]
 
-connect_bd_intf_net [get_bd_intf_pins axi_quad_spi/AXI_LITE] -boundary_type upper [get_bd_intf_pins axi_interconnect/M05_AXI]
-connect_bd_net [get_bd_pins axi_quad_spi/s_axi_aclk] [get_bd_pins clk_wiz_1/clk_out1]
-connect_bd_net [get_bd_pins axi_quad_spi/s_axi_aresetn] [get_bd_pins sys_rstgen/peripheral_aresetn]
-connect_bd_net [get_bd_pins axi_quad_spi/ext_spi_clk] [get_bd_pins clk_wiz_1/clk_out1]
+connect_bd_intf_net [get_bd_intf_pins axi_spi/AXI_LITE] -boundary_type upper [get_bd_intf_pins axi_interconnect/M05_AXI]
+connect_bd_net [get_bd_pins axi_spi/s_axi_aclk] [get_bd_pins clk_wiz_1/clk_out1]
+connect_bd_net [get_bd_pins axi_spi/s_axi_aresetn] [get_bd_pins sys_rstgen/peripheral_aresetn]
+connect_bd_net [get_bd_pins axi_spi/ext_spi_clk] [get_bd_pins clk_wiz_1/clk_out1]
 
 create_bd_intf_port -mode Slave -vlnv xilinx.com:interface:aximm_rtl:1.0 S_AXI
 set_property -dict [list CONFIG.ID_WIDTH [get_property CONFIG.ID_WIDTH [get_bd_intf_pins axi_interconnect/xbar/S02_AXI]] CONFIG.HAS_REGION [get_property CONFIG.HAS_REGION [get_bd_intf_pins axi_interconnect/xbar/S02_AXI]] CONFIG.NUM_READ_OUTSTANDING [get_property CONFIG.NUM_READ_OUTSTANDING [get_bd_intf_pins axi_interconnect/xbar/S02_AXI]] CONFIG.NUM_WRITE_OUTSTANDING [get_property CONFIG.NUM_WRITE_OUTSTANDING [get_bd_intf_pins axi_interconnect/xbar/S02_AXI]]] [get_bd_intf_ports S_AXI]
@@ -225,12 +233,23 @@ connect_bd_intf_net [get_bd_intf_pins axi_ethernetlite/S_AXI] -boundary_type upp
 connect_bd_net [get_bd_pins axi_ethernetlite/s_axi_aclk] [get_bd_pins clk_wiz_1/clk_out1]
 connect_bd_net [get_bd_pins axi_ethernetlite/s_axi_aresetn] [get_bd_pins sys_rstgen/peripheral_aresetn]
 
+create_bd_port -dir I -type clk -freq_hz 100000000 ext_spi_clk
+connect_bd_net [get_bd_pins /axi_quad_spi/ext_spi_clk] [get_bd_ports ext_spi_clk]
+create_bd_intf_port -mode Master -vlnv xilinx.com:display_startup_io:startup_io_rtl:1.0 QSPI_STARTUP_IO
+connect_bd_intf_net [get_bd_intf_pins axi_quad_spi/STARTUP_IO] [get_bd_intf_ports QSPI_STARTUP_IO]
+create_bd_intf_port -mode Master -vlnv xilinx.com:interface:spi_rtl:1.0 QSPI_0
+connect_bd_intf_net [get_bd_intf_pins axi_quad_spi/SPI_0] [get_bd_intf_ports QSPI_0]
+connect_bd_intf_net -boundary_type upper [get_bd_intf_pins axi_interconnect/M09_AXI] [get_bd_intf_pins axi_quad_spi/AXI_LITE]
+connect_bd_net [get_bd_pins axi_quad_spi/s_axi_aclk] [get_bd_pins clk_wiz_1/clk_out1]
+connect_bd_net [get_bd_pins axi_quad_spi/s_axi_aresetn] [get_bd_pins sys_rstgen/interconnect_aresetn]
+connect_bd_net [get_bd_pins axi_quad_spi/ip2intc_irpt] [get_bd_pins irq_concat/In4]
+
 create_bd_port -dir I -from 2 -to 0 -type intr IRQ_F2P
 connect_bd_net [get_bd_ports IRQ_F2P] [get_bd_pins irq_concat/In0]
 
 connect_bd_net [get_bd_pins axi_uartlite/interrupt] [get_bd_pins irq_concat/In1]
 connect_bd_net [get_bd_pins axi_ethernetlite/ip2intc_irpt] [get_bd_pins irq_concat/In2]
-connect_bd_net [get_bd_pins axi_quad_spi/ip2intc_irpt] [get_bd_pins irq_concat/In3]
+connect_bd_net [get_bd_pins axi_spi/ip2intc_irpt] [get_bd_pins irq_concat/In3]
 
 connect_bd_net [get_bd_pins xlconstant_0/dout] [get_bd_pins irq_concat/In29]
 connect_bd_net [get_bd_pins irq_concat/In28] [get_bd_pins xlconstant_0/dout]
@@ -256,8 +275,7 @@ connect_bd_net [get_bd_pins irq_concat/In9] [get_bd_pins xlconstant_0/dout]
 connect_bd_net [get_bd_pins irq_concat/In8] [get_bd_pins xlconstant_0/dout]
 connect_bd_net [get_bd_pins irq_concat/In7] [get_bd_pins xlconstant_0/dout]
 connect_bd_net [get_bd_pins irq_concat/In6] [get_bd_pins xlconstant_0/dout]
-connect_bd_net [get_bd_pins irq_concat/In5] [get_bd_pins xlconstant_0/dout]
-connect_bd_net [get_bd_pins irq_concat/In4] [get_bd_pins xlconstant_0/dout]
+# connect_bd_net [get_bd_pins irq_concat/In5] [get_bd_pins xlconstant_0/dout]
 
 connect_bd_net [get_bd_pins irq_concat/dout] [get_bd_pins inst_ruffle_axi_bscane/plicInterrupts]
 
@@ -283,7 +301,7 @@ assign_bd_address
 # exclude_bd_addr_seg [get_bd_addr_segs axi_ddr_ctrl/memmap/memaddr] -target_address_space [get_bd_addr_spaces inst_ruffle_axi_bscane/m_axi_ibus]
 exclude_bd_addr_seg [get_bd_addr_segs inst_ruffle_axi_bscane/m_axi_ibus/SEG_axi_ethernetlite_Reg]
 exclude_bd_addr_seg [get_bd_addr_segs inst_ruffle_axi_bscane/m_axi_ibus/SEG_axi_gpio_Reg]
-exclude_bd_addr_seg [get_bd_addr_segs inst_ruffle_axi_bscane/m_axi_ibus/SEG_axi_quad_spi_Reg]
+exclude_bd_addr_seg [get_bd_addr_segs inst_ruffle_axi_bscane/m_axi_ibus/SEG_axi_spi_Reg]
 exclude_bd_addr_seg [get_bd_addr_segs inst_ruffle_axi_bscane/m_axi_ibus/SEG_axi_uartlite_Reg]
 exclude_bd_addr_seg [get_bd_addr_segs inst_ruffle_axi_bscane/m_axi_ibus/SEG_inst_ruffle_axi_bscane_reg0]
 exclude_bd_addr_seg [get_bd_addr_segs inst_ruffle_axi_bscane/m_axi_ibus/SEG_inst_ruffle_axi_bscane_reg0_1]
